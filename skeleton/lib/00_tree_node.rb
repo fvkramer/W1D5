08e0d9343943a2ec5b094f9583f87049c061df5a
fvkramer <krame490@gmail.com>
2018-10-26 13:42:23 -0700

    Modify reassignment function for #parent=

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -6,8 +6,27 @@ class PolyTreeNode
     @children = []
   end
   
+  def parent=(parent_node)
+    if parent_node == nil
+      if @parent == nil
+        return @parent
+      else
+        @parent.children.delete(self)
+        return @parent = parent_node
+      end
+    end
+    
+    if @parent == nil
+      @parent = parent_node
+      parent_node.children << self
+    else
+      @parent.children.delete(self)
+      @parent = parent_node
+      parent_node.children << self
+    end 
+  end
   
   
-  private
-  attr_accessor :value, :children, :parent
+  # private
+  attr_reader :value, :children, :parent
 end
