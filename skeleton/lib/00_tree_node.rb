a019f99bf6ba0c3c1fe4ea7ee07f4c161408322b
fvkramer <krame490@gmail.com>
2018-10-26 11:42:04 -0700

    Add accessor value, children, parent

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,13 @@
 class PolyTreeNode
-
+  
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+  
+  
+  
+  private
+  attr_accessor :value, :children, :parent
 end
