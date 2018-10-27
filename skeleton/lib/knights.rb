07973fda8876b3dc5bd024877781fd82241df745
fvkramer <krame490@gmail.com>
2018-10-26 18:10:39 -0700

    Add knights.rb

---
+++ skeleton/lib/knights.rb
@@ -0,0 +1,26 @@
+class KnightPathFinder
+  
+  def self.valid_moves(pos)
+    total_steps = 3
+    if pos[0] 
+  end
+  
+  def valid_pos
+    
+  end
+  
+  def initialize(root_node = [0,0])
+    @root_node = root_node
+    build_move_tree(root_node)
+  end
+  
+  #somewhere we need an instance of a polytreenode
+  def build_move_tree(start_pos)
+    # generate next possible move
+    
+  end
+  
+  def find_path
+    
+  end
+end
